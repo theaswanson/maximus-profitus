3e769671b38002bc5f3ff9516b2944d338a72ee1
Adam Swanson <adam.swanson.98@gmail.com>
2023-08-08 20:05:36 -0400

    Register IProductService and ISquareClient

--- backend/MaximusProfitus.API/Program.cs
+++ backend/MaximusProfitus.API/Program.cs
@@ -1,3 +1,5 @@
+using MaximusProfitus.Core.Products;
+using Square;
 
 namespace MaximusProfitus.API
 {
@@ -13,6 +15,16 @@ namespace MaximusProfitus.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Configuration.AddUserSecrets<Program>();
+            }
+
+            var squareClient = BuildSquareClient(builder.Environment, builder.Configuration);
+
+            builder.Services.AddSingleton<ISquareClient>(squareClient);
+            builder.Services.AddTransient<IProductService, SquareProductService>();
 
             var app = builder.Build();
 
@@ -31,6 +43,25 @@ namespace MaximusProfitus.API
             app.MapControllers();
 
             app.Run();
+
+            static SquareClient BuildSquareClient(IWebHostEnvironment webHostEnvironment, IConfigurationRoot configuration)
+            {
+                var squareEnvironment = webHostEnvironment.IsDevelopment()
+                    ? Square.Environment.Sandbox
+                    : Square.Environment.Production;
+
+                var accessToken = configuration["Square:AccessToken"];
+
+                if (string.IsNullOrEmpty(accessToken))
+                {
+                    throw new Exception("Invalid Square access token. Ensure your access token is properly configured.");
+                }
+
+                return new SquareClient.Builder()
+                    .Environment(squareEnvironment)
+                    .AccessToken(accessToken)
+                    .Build();
+            }
         }
     }
 }
